bf2e2a39f22f552f2a7c92ce7c8dca7795bd3537
João Maria Janeiro <joaomariajaneiro1@gmail.com>
2020-06-27 22:50:00 +0100

    Fixed url for non ascii

--- saoperolasrest/products/urls.py
+++ saoperolasrest/products/urls.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('cover-photos/', csrf_exempt(views.get_cover_photos), name="get_cover_photos"),
     path('background/', csrf_exempt(views.get_background_photo), name="background"),
-    path('get/<slug:p_type>/', csrf_exempt(views.get_products), name="get_products"),
+    path('get/<str:p_type>/', csrf_exempt(views.get_products), name="get_products"),
     path('types/', csrf_exempt(views.get_types), name="types"),
     path('create/', csrf_exempt(views.create_product), name="create_product"),
     path('details/<int:id>/', csrf_exempt(views.get_product_from_id), name="get_product_from_id"),
